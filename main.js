58a5ba165f909d5b5783630f30ae1629f9bb313d
Stella Lee <stjilee@ucsc.edu>
2015-02-05 17:09:23 -0800

    whatamidoing

---
+++ main.js
@@ -0,0 +1,9 @@
+use2D = true;
+
+var mySprite = new Sprite();
+
+mySprite.width = 100;
+mySprite.height = 100;
+
+mySprite.image = Textures.load("http://i.imgur.com/v4g3j0c.png");
+world.addChild(mySprite);
